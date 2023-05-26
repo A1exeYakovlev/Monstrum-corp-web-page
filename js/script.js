4db5de857fe794738d05d8058c9483578c29ceea
A1exeYakovlev <118552675+A1exeYakovlev@users.noreply.github.com>
2023-05-26 13:25:49 +0300

    Feat: provide navigation and smooth scrolling

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
 const mobMenuAllBtnEl = document.querySelectorAll(".mob-menu-btn");
 
-for (let i = 0; i < mobMenuAllBtnEl.length; i++) {
-  mobMenuAllBtnEl[i].addEventListener("click", function () {
+//Menu links functionality
+document.querySelectorAll("nav").forEach(function (el) {
+  el.addEventListener("click", function (event) {
+    //Smooth scrolling
+    event.preventDefault();
+    if (event.target.classList.contains("menu-link")) {
+      const id = event.target.getAttribute("href");
+      if (id === "#") {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      } else if (id.startsWith("#")) {
+        document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+      }
+    }
+
+    //Close mob menu after click on a link
+    if (document.querySelector("html").classList.contains("open-mob-menu")) {
+      document.querySelector("html").classList.toggle("open-mob-menu");
+    }
+  });
+});
+
+//Open and close mobile menu by button
+mobMenuAllBtnEl.forEach(function (el) {
+  el.addEventListener("click", function () {
     document.querySelector("html").classList.toggle("open-mob-menu");
   });
-}
+});
